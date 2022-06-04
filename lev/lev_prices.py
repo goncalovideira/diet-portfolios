43b8af223637cfcddc93e832680924c7634d64b1
Goncalo Videira <gvideira@byzapplabs.com>
2022-06-04 22:34:49 +0100

    python script and json file with schema

---
+++ lev/lev_prices.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import pandas as pd
+import os
+
+# setup URL and HEADERS | per page max is 100, NEED TO CHANGE WHEN SCRIPT IS WORKING
+url = "https://levcms.live.afonso.se/wp-json/wc/v3/products?category=22&per_page=2&page=1&hide_empty=true&catalog_visibility=visible"
+payload={}
+headers = {
+  'authority': 'levcms.live.afonso.se',
+  'accept': 'application/json, text/plain, */*',
+  'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+  'authorization': 'Basic Y2tfNTZjZTY0YjgwYTExNDE5N2QyYzVhZWU4NGFlMDA0NjJhZWMzYTM4MDpjc19jZWRhMDczYTljOGFjNDlhNzY5NmVkMmJiZDQ3ZmQwYjVjNDBkYjRk',
+  'origin': 'https://lev.pt',
+  'referer': 'https://lev.pt/',
+  'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"Windows"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'cross-site',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
+}
+
+# get response
+response = requests.request("GET", url, headers=headers, data=payload)
+
+# write to file
+with open("tempData.json", "w") as outfile:
+    json.dump(response.json(), outfile)
+
+# create Dataframe
+df = pd.read_json("tempData.json")
+
+# delete file
+os.remove("tempData.json")
+
+#print results
+print(df.columns)
+print(df['price'])
