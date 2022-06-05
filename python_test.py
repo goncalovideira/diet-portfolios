5fcec723d60fdbbb8369931a21b4125a5b194d55
Gonçalo Videira <97890512+goncalovideira@users.noreply.github.com>
2022-06-05 20:01:52 +0100

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,7 +1,9 @@
 import os
 
-for k, v in sorted(os.environ.items()):
-    print(k+':', v)
-print('\n')
+print(os.environ['LEV_TOKEN'])
+
+#for k, v in sorted(os.environ.items()):
+#    print(k+':', v)
+#print('\n')
 # list elements in path environment variable
-[print(item) for item in os.environ['PATH'].split(';')]
+#[print(item) for item in os.environ['PATH'].split(';')]
