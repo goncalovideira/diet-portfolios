956c5dea7adb5e76da5129e4e74960e53b9e592d
Gonçalo Videira <97890512+goncalovideira@users.noreply.github.com>
2022-06-05 19:41:39 +0100

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,7 @@
+import os
+
+for k, v in sorted(os.environ.items()):
+    print(k+':', v)
+print('\n')
+# list elements in path environment variable
+[print(item) for item in os.environ['PATH'].split(';')]
