17e7fb304fc63918e88688e29e8d10aadc278cac
Goncalo Videira <gvideira@byzapplabs.com>
2022-06-05 20:23:38 +0100

    deleted unused files

--- python_test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(os.environ['LEV_TOKEN'])
-
-#for k, v in sorted(os.environ.items()):
-#    print(k+':', v)
-#print('\n')
-# list elements in path environment variable
-#[print(item) for item in os.environ['PATH'].split(';')]
